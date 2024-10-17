05025c7ceb52e4aadaac7562e12e37a505baa5b0
MariaGonzalez <maria_angelica.gonzalez_alarcon@lcieducation.net>
2024-10-17 10:52:18 -0400

    delete dashboard class and xml, and change home class and home xml

--- app/src/main/java/com/example/taskmanager_project/Home.java
+++ app/src/main/java/com/example/taskmanager_project/Home.java
@@ -6,31 +6,54 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
 
-    Button editProfile, taskManagment, dashboard, logout;
+    Button editProfile, taskManagment, users, roles, logout;
     Spinner spinner;
     TextView textName, infoText;
     ImageView imgProfile;
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+    private DatabaseReference userDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+        userDatabase = FirebaseDatabase.getInstance().getReference("users");
+
         editProfile = findViewById(R.id.btnEditProfile);
         taskManagment = findViewById(R.id.btnTasks);
-        dashboard = findViewById(R.id.btnDashboard);
+        users = findViewById(R.id.btnUsers);
+        roles = findViewById(R.id.btnRoles);
         logout = findViewById(R.id.logout);
         //spinner = findViewById(R.id.spinner);
         textName = findViewById(R.id.nametxt);
         infoText = findViewById(R.id.personaltxt);
         imgProfile = findViewById(R.id.topImage);
+
+        users.setVisibility(View.GONE);
+        roles.setVisibility(View.GONE);
+        if (currentUser != null) {
+            loadUserRole();
+        }
 
 
         editProfile.setOnClickListener(new View.OnClickListener() {
@@ -53,11 +76,21 @@ public class Home extends AppCompatActivity {
             }
         });
 
-        dashboard.setOnClickListener(new View.OnClickListener() {
+        users.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Home.this,
-                        Dashboard.class);
+                        Manage_User_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        roles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this,
+                        Manage_Roles_Activity.class);
                 startActivity(intent);
                 finish();
             }
@@ -74,4 +107,31 @@ public class Home extends AppCompatActivity {
         });
 
     }
+
+    private void loadUserRole() {
+        String userId = currentUser.getUid();
+
+        userDatabase.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String role = snapshot.child("role").getValue(String.class);
+
+                    assert role != null;
+                    if (role.equals("Project Manager")) {
+                            users.setVisibility(View.VISIBLE);
+                            roles.setVisibility(View.VISIBLE);
+                        }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Home.this, "Failed to load user data", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
 }
